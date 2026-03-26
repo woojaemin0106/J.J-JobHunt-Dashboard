5519b5097448300ea717e6a032881e0a1c896f43
woojaemin0106 <dnwoals19@naver.com>
2026-03-26 23:27:40 +0900

    feat: add route guards and remove duplicate auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,53 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
-import Home from "./pages/Home";
 import Applications from "./pages/Applications";
-import Resume from "./pages/Resume";
-import Notes from "./pages/Notes";
-import Statistics from "./pages/Statistics";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Notes from "./pages/Notes";
+import Resume from "./pages/Resume";
 import Signup from "./pages/Signup";
-import { AuthProvider } from "./store/authStore";
+import Statistics from "./pages/Statistics";
+import { ProtectedRoute, PublicOnlyRoute } from "./routes/RouteGuards";
 
 export default function App() {
   return (
-    <AuthProvider>
     <Routes>
-      {/* 로그인/회원가입 페이지는 AppLayout 없이 */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      
-      {/* 나머지 페이지는 AppLayout 사용 */}
+      {/* 공개 라우트 */}
+      <Route
+        path="/login"
+        element={
+          <PublicOnlyRoute>
+            <Login />
+          </PublicOnlyRoute>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <PublicOnlyRoute>
+            <Signup />
+          </PublicOnlyRoute>
+        }
+      />
+
+      {/* 보호 라우트 */}
       <Route
         path="/*"
         element={
-          <AppLayout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/applications" element={<Applications />} />
-              <Route path="/resume" element={<Resume />} />
-              <Route path="/notes" element={<Notes />} />
-              <Route path="/statistics" element={<Statistics />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </AppLayout>
+          <ProtectedRoute>
+            <AppLayout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/applications" element={<Applications />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="/notes" element={<Notes />} />
+                <Route path="/statistics" element={<Statistics />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </AppLayout>
+          </ProtectedRoute>
         }
       />
     </Routes>
-    </AuthProvider>
   );
 }
